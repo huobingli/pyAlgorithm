240e27212d85f02a2689a35227e95a1d2da30d43
huobingli <huobingli@outlook.com>
2020-03-24 23:56:54 +0800

    singleton

---
+++ singleton.py
@@ -0,0 +1,16 @@
+from functools import wraps
+
+
+def singleton(cls):
+    """装饰类的装饰器"""
+    instances = {}
+
+    @wraps(cls)
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper
+
+@singleton
